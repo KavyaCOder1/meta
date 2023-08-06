2227c9bdf5121f2678fefabf4298ead1338f946a
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2023-08-06 15:27:23 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: '5836906160:AAGbdNUBbXHjf7bmzAZaQA5H0NX2FYnqmuM',
+    token: '6370955978:AAFCtdDM_7VeI9XFcShd3ScrAJgDT9GlcsY',
     admins: [5556717138],
     mongo_url: 'mongodb+srv://manishprajapati7517:R4gO0q3MFPDbjqKp@kavyatg.zgsh5nz.mongodb.net/?retryWrites=true&w=majority',
-    medialink: 'https://graph.org/file/aca9854398236a03783ec.jpg'
+    medialink: 'https://graph.org/file/7e3ce6523c1bffb53b507.jpg'
 }
 
